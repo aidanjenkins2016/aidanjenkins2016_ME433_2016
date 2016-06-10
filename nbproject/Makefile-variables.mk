ac7ff8c02259d675a82bc6f30677eac4b7c778b9
aidanjenkins2016 <aidanjenkins2016@u.northwestern.edu>
2016-06-09 19:08:00 -0500

    Added HW16

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=aidanjenkins2016_ME433_2016.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/aidanjenkins2016_ME433_2016.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=aidanjenkins2016me4332016.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/aidanjenkins2016me4332016.tar
